3f667e87146dc6ea90dfce1017ce794803a98d57
ahstevens <ahstevens@gmail.com>
2016-07-22 16:48:47 -0400

    small tweak

--- VRSonarCleaner/SonarPointCloud.cpp
+++ VRSonarCleaner/SonarPointCloud.cpp
@@ -563,11 +563,11 @@ bool SonarPointCloud::checkForHit(Vector3 begin, Vector3 end, float radius)
 	bool hit = false;
 	Vector3 thisPt;
 	
-	float dist, tempRad = 100.f;
+	float dist, tempRad = 1.f;
 
 	for (int i = 0; i < numPoints; i += 3)
 	{
-		thisPt = Vector3(pointsPositions[i], pointsPositions[i + 2], pointsPositions[i + 1]); // SWAP Y AND Z
+		thisPt = Vector3(pointsPositions[i], pointsPositions[i + 1], pointsPositions[i + 2]); // SWAP Y AND Z
 		//dist = distFromPointToLineSegment(thisPt, begin, end);
 		dist = (thisPt - end).length();
 		
@@ -590,10 +590,6 @@ bool SonarPointCloud::checkForHit(Vector3 begin, Vector3 end, float radius)
 	pointsPositions[(numPoints - 1) * 3 + 0] = end.x;
 	pointsPositions[(numPoints - 1) * 3 + 1] = end.y;
 	pointsPositions[(numPoints - 1) * 3 + 2] = end.z;
-
-	pointsColors[(numPoints - 1) * 3 + 0] = 0.f;
-	pointsColors[(numPoints - 1) * 3 + 1] = 0.f;
-	pointsColors[(numPoints - 1) * 3 + 2] = 1.f;
 
 	refreshNeeded = true;
 
