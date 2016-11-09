21f359a2ceb044cdafc3bb29e13d40a0e1e877a8
ahstevens <ahstevens@gmail.com>
2016-11-09 12:56:26 -0500

    initial commit of Texture class and info box manager

---
+++ VRSonarCleaner/TexturedQuadManager.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <openvr.h>
+
+#include "shared/Texture.h"
+#include "ViveController.h"
+#include "EditingController.h"
+
+
+class TexturedQuadManager
+{
+public:
+	TexturedQuadManager(vr::IVRSystem* pHMD);
+	~TexturedQuadManager();
+
+	bool BInit();
+
+	void handleEvents();
+
+	void processVREvent(const vr::VREvent_t & event);
+
+	void updateControllerStates();
+
+	bool cleaningModeActive();
+	bool getCleaningCursorData(Matrix4 *thisCursorPose, Matrix4 *lastCursorPose, float *radius);
+	bool getManipulationData(Matrix4 &controllerPose);
+	void cleaningHit();
+
+	void renderTrackedDevices(Matrix4 & matVP);
+	void postRenderUpdate();
+
+	Matrix4 & getHMDPose();
+
+	void UpdateHMDMatrixPose();
+private:
+	void initDevices();
+	void setupTrackedDevice(vr::TrackedDeviceIndex_t unTrackedDeviceIndex);
+	
+	vr::IVRSystem *m_pHMD;
+	vr::IVRRenderModels *m_pRenderModels;
+
+	TrackedDevice* m_rpTrackedDevices[vr::k_unMaxTrackedDeviceCount];
+	EditingController* m_pEditController;
+	ViveController* m_pManipController;
+
+	int m_iValidPoseCount;
+	int m_iValidPoseCount_Last;
+	int m_iTrackedControllerCount;
+	int m_iTrackedControllerCount_Last;
+
+	std::string m_strPoseClasses;                         // what classes we saw poses for this frame
+
+	Matrix4 m_mat4HMDPose;
+};
+
---
+++ shared/Texture.h
@@ -0,0 +1,66 @@
+#include <GL\glew.h>
+#include <iostream>
+#include <string>
+#include "shared/lodepng.h"
+
+class Texture
+{
+public:
+	Texture(std::string png_filename)
+		: m_uiID(0)
+		, m_uiWidth(0)
+		, m_uiHeight(0)
+	{
+		m_strFileName = png_filename;
+		
+		// Load file and decode image.
+		std::vector<unsigned char> image;
+
+		unsigned error = lodepng::decode(image, m_uiWidth, m_uiHeight, m_strFileName);
+
+		if (error != 0)
+			std::cerr << "error " << error << ": " << lodepng_error_text(error) << std::endl;
+
+		// Generate texture
+		glGenTextures(1, &m_uiID);
+		glBindTexture(GL_TEXTURE_2D, m_uiID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexImage2D(GL_TEXTURE_2D,
+			0,
+			GL_RGBA,
+			m_uiWidth,
+			m_uiHeight,
+			0,
+			GL_RGBA,
+			GL_UNSIGNED_BYTE,
+			&image[0]);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+	~Texture()
+	{
+		glDeleteTextures(1, &m_uiID);
+	}
+
+	unsigned getWidth() { return m_uiWidth; }
+	unsigned getHeight() { return m_uiHeight; }
+
+	void activate()
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, m_uiID);
+	}
+
+	void deactivate()
+	{
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+private:
+	GLuint m_uiID;
+	std::string m_strFileName;
+	unsigned m_uiWidth, m_uiHeight;
+};
