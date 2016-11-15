4e950133c2775a27ffa64a0e9275d889293ba8df
tbutkie <tbutkiewicz@ccom.unh.edu>
2016-11-15 11:19:24 -0500

    added desktop mode

---
+++ VRSonarCleaner/LassoWindow.h
@@ -0,0 +1,124 @@
+#pragma once
+#pragma once
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <gl/glu.h>
+#include <stdio.h>
+#include <string>
+#include <cstdlib>
+
+#include "CleaningRoom.h"
+#include "CloudCollection.h"
+//#include "FocalCamera.h"
+#include "DataVolume.h"
+#include "arcball.h"
+
+#include "../shared/lodepng.h"
+#include "../shared/Matrices.h"
+#include "../shared/pathtools.h"
+
+
+class LassoWindow
+{
+public:
+	LassoWindow(int argc, char *argv[]);
+	virtual ~LassoWindow();
+
+	void dprintf(const char *fmt, ...);
+
+	bool BInit();
+	bool BInitGL();
+	bool BInitCompositor();
+
+	void Shutdown();
+
+	void recalcVolumeBounds();
+
+	void RunMainLoop();
+	bool HandleInput();
+	void RenderFrame();
+
+	void perRenderUpdates();
+
+	void display();
+		
+	CleaningRoom* cleaningRoom;
+
+	//FocalCamera *cam;
+
+private:
+	
+	//ARCBALL STUFF
+	// scene parameters
+	vec ballEye;
+	vec ballCenter;
+	vec ballUp;
+	float ballRadius;
+	
+	DataVolume *dataVolume;
+
+	bool leftMouseDown;
+
+	bool m_bDebugOpenGL;
+	bool m_bVerbose;
+	bool m_bPerf;
+	bool m_bVblank;
+	bool m_bGlFinishHack;
+
+private: // SDL bookkeeping
+	SDL_Window *m_pWindow;
+	uint32_t m_nWindowWidth;
+	uint32_t m_nWindowHeight;
+
+	SDL_GLContext m_pContext;
+
+private: // OpenGL bookkeeping
+	float m_fNearClip;
+	float m_fFarClip;
+
+	GLuint m_unLensVAO;
+	GLuint m_glIDVertBuffer;
+	GLuint m_glIDIndexBuffer;
+	unsigned int m_uiIndexSize;
+
+	Matrix4 m_mat4eyePosLeft;
+	Matrix4 m_mat4eyePosRight;
+
+	Matrix4 m_mat4ProjectionCenter;
+	Matrix4 m_mat4ProjectionLeft;
+	Matrix4 m_mat4ProjectionRight;
+
+	struct VertexDataScene
+	{
+		Vector3 position;
+		Vector2 texCoord;
+	};
+
+	struct VertexDataLens
+	{
+		Vector2 position;
+		Vector2 texCoordRed;
+		Vector2 texCoordGreen;
+		Vector2 texCoordBlue;
+	};
+
+	GLuint m_unLensProgramID;
+
+	struct FramebufferDesc
+	{
+		GLuint m_nDepthBufferId;
+		GLuint m_nRenderTextureId;
+		GLuint m_nRenderFramebufferId;
+		GLuint m_nResolveTextureId;
+		GLuint m_nResolveFramebufferId;
+	};
+	FramebufferDesc leftEyeDesc;
+	FramebufferDesc rightEyeDesc;
+
+	bool CreateFrameBuffer(int nWidth, int nHeight, FramebufferDesc &framebufferDesc);
+
+	uint32_t m_nRenderWidth;
+	uint32_t m_nRenderHeight;
+};
--- VRSonarCleaner/SonarCleanerMain.cpp
+++ VRSonarCleaner/SonarCleanerMain.cpp
@@ -2,6 +2,7 @@
 #include "SonarPointCloud.h"
 #include "ColorScaler.h"
 #include "CloudCollection.h"
+#include "LassoWindow.h"
 
 CloudCollection *clouds;
 ColorScaler *colorScalerTPU;
@@ -21,29 +22,53 @@ int main(int argc, char *argv[])
 
 	clouds = new CloudCollection();
 	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_1085.txt");
-	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_528_1324.txt");
-	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1516.txt");
-	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1508.txt");
-	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1500.txt");
-//	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_XL_901_1458.txt");  //TO BIG AND LONG at 90 degree angle to others
-	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_148_000_2022.txt");	
+	//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_528_1324.txt");
+	//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1516.txt");
+	//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1508.txt");
+	//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1500.txt");
+///	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_XL_901_1458.txt");  //TO BIG AND LONG at 90 degree angle to others
+	//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_148_000_2022.txt");	
 	clouds->calculateCloudBoundsAndAlign();
 	
-	CMainApplication *pMainApplication = new CMainApplication( argc, argv );
+	bool lassoMode = false;
 
-	if (!pMainApplication->BInit())
+	if (lassoMode)
 	{
-		pMainApplication->Shutdown();
-		return 1;
+		LassoWindow *lassoWindow = new LassoWindow(argc, argv);
+
+		if (!lassoWindow->BInit())
+		{
+			lassoWindow->Shutdown();
+			return 1;
+		}
+
+		lassoWindow->RunMainLoop();
+
+		//this doesnt work here?
+		fclose(stdout);
+		FreeConsole();
+
+		lassoWindow->Shutdown();
+
 	}
+	else //run regular VR Sonar Cleaner
+	{
+		CMainApplication *pMainApplication = new CMainApplication(argc, argv);
 
-	pMainApplication->RunMainLoop();
+		if (!pMainApplication->BInit())
+		{
+			pMainApplication->Shutdown();
+			return 1;
+		}
 
-	//this doesnt work here?
-	fclose(stdout);
-	FreeConsole();
+		pMainApplication->RunMainLoop();
 
-	pMainApplication->Shutdown();
+		//this doesnt work here?
+		fclose(stdout);
+		FreeConsole();
+
+		pMainApplication->Shutdown();
+	}
 
 	exit(0);
 	return 0;
