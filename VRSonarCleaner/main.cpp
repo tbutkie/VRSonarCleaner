a7fbf3c42099827dd7853dde92da8093fdde893b
ahstevens <ahstevens@gmail.com>
2017-03-16 23:44:04 -0400

    simplify main

--- VRSonarCleaner/main.cpp
+++ VRSonarCleaner/main.cpp
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
 	AllocConsole();
 	freopen("CONOUT$", "wb", stdout);
 
-	printf("Select Mode: (0=VR Cleaner) (1=VR Flow) (2=Study-VR) (3=Study-Desktop\n");
+	printf("Select Mode:\n\t0=VR Cleaner\n\t1=VR Flow\n\t2=Study-VR\n\t3=Study-Desktop\n");
 	char selectedMode;
 	selectedMode = getch();
 	//scanf("%d", &selectedMode);
 
 
 	//bool lassoMode = false;
-	if (selectedMode == '0') //VR Cleaner
+	if (selectedMode == '0' || selectedMode == '2' || selectedMode == '3') //VR Cleaner
 	{
 		colorScalerTPU = new ColorScaler();
 		colorScalerTPU->setColorScale(2);
@@ -40,8 +40,11 @@ int main(int argc, char *argv[])
 		///	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_XL_901_1458.txt");  //TO BIG AND LONG at 90 degree angle to others
 		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_148_000_2022.txt");	
 		clouds->calculateCloudBoundsAndAlign();
+	}
 
-		CMainApplication *pMainApplication = new CMainApplication(argc, argv, 0);
+	if (selectedMode == '0' || selectedMode == '1' || selectedMode == '2')
+	{
+		CMainApplication *pMainApplication = new CMainApplication(argc, argv, selectedMode % 2);
 
 		if (!pMainApplication->BInit())
 		{
@@ -57,72 +60,9 @@ int main(int argc, char *argv[])
 
 		pMainApplication->Shutdown();
 	}
-	else if (selectedMode == '1') //VR Flow
+	
+	if (selectedMode == '3')
 	{
-		CMainApplication *pMainApplication = new CMainApplication(argc, argv, 1);
-
-		if (!pMainApplication->BInit())
-		{
-			pMainApplication->Shutdown();
-			return 1;
-		}
-
-		pMainApplication->RunMainLoop();
-
-		//this doesnt work here?
-		fclose(stdout);
-		FreeConsole();
-
-		pMainApplication->Shutdown();
-	}
-	else if (selectedMode == '2') //Study VR
-	{
-		colorScalerTPU = new ColorScaler();
-		colorScalerTPU->setColorScale(2);
-		colorScalerTPU->setBiValueScale(1);
-
-		clouds = new CloudCollection();
-		clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_1085.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_528_1324.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1516.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1508.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1500.txt");
-		///	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_XL_901_1458.txt");  //TO BIG AND LONG at 90 degree angle to others
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_148_000_2022.txt");	
-		clouds->calculateCloudBoundsAndAlign();
-
-		CMainApplication *pMainApplication = new CMainApplication(argc, argv, 0);
-
-		if (!pMainApplication->BInit())
-		{
-			pMainApplication->Shutdown();
-			return 1;
-		}
-
-		pMainApplication->RunMainLoop();
-
-		//this doesnt work here?
-		fclose(stdout);
-		FreeConsole();
-
-		pMainApplication->Shutdown();
-	}
-	else if (selectedMode == '3')
-	{
-		colorScalerTPU = new ColorScaler();
-		colorScalerTPU->setColorScale(2);
-		colorScalerTPU->setBiValueScale(1);
-
-		clouds = new CloudCollection();
-		clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_1085.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-267_267_528_1324.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1516.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1508.txt");
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-149_149_000_1500.txt");
-		///	clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_XL_901_1458.txt");  //TO BIG AND LONG at 90 degree angle to others
-		//clouds->loadCloud("H12676_TJ_3101_Reson7125_SV2_400khz_2014_2014-148_148_000_2022.txt");	
-		clouds->calculateCloudBoundsAndAlign();
-
 		LassoWindow *lassoWindow = new LassoWindow(argc, argv);
 
 		if (!lassoWindow->BInit())
