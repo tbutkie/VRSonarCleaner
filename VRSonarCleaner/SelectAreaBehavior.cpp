1bceb308dc5c9914f3f600391c73789de988da0d
ahstevens <ahstevens@gmail.com>
2017-09-29 14:49:28 -0400

    selection area implemented

--- VRSonarCleaner/SelectAreaBehavior.cpp
+++ VRSonarCleaner/SelectAreaBehavior.cpp
@@ -8,8 +8,13 @@ SelectAreaBehavior::SelectAreaBehavior(ViveController* primaryController, ViveCo
 	: DualControllerBehavior(primaryController, secondaryController)
 	, m_pDataVolumeSelection(selectionVolume)
 	, m_pDataVolumeDisplay(displayVolume)
+	, m_bActive(false)
 	, m_bShowCursor(false)
 	, m_bSelectingArea(false)
+	, m_bMovingArea(false)
+	, m_bCustomAreaSet(false)
+	, m_bRayHitPlane(false)
+	, m_bRayHitDomain(false)
 	, m_vec3CursorSize(glm::vec3(0.01f, 0.01f, 0.001f))
 {
 }
@@ -21,76 +26,135 @@ SelectAreaBehavior::~SelectAreaBehavior()
 
 void SelectAreaBehavior::update()
 {
-	glm::vec3 planeOrigin = m_pDataVolumeSelection->getPosition() + glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 1.f)) * m_pDataVolumeSelection->getDimensions().z * 0.5f;
-	glm::vec3 planeNormal = glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 1.f));
-	glm::vec3 rayOrigin = glm::vec3(m_pPrimaryController->getPose()[3]);
-	glm::vec3 rayDirection = glm::normalize(glm::vec3(-m_pPrimaryController->getPose()[2]));
+	glm::vec3 hitPt = calcHits();
 
-	bool rayHitDataVolume = castRay(rayOrigin, rayDirection, planeOrigin, planeNormal, &m_vec3CurrentLocationOnPlane);
-	bool rayHitInDomain = rayHitDataVolume ? m_pDataVolumeSelection->isWorldCoordPointInBounds(m_vec3CurrentLocationOnPlane, false) : false;
+	if (m_bRayHitPlane)
+		m_vec3CurrentLocationOnPlane = hitPt;
 
-	if (rayHitInDomain)
+	if (m_bRayHitDomain)
+	{		
 		m_bShowCursor = true;
+	}
 	else
-		m_bShowCursor = false;
-
-	if (m_bSelectingArea && rayHitInDomain)
 	{
+		m_bShowCursor = false;
+	}
 
-		glm::dvec3 tmpStart = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3StartLocationOnPlane);
-		glm::dvec3 tmpNow = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3CurrentLocationOnPlane);
 
-		m_dvec3MinBound.x = std::min(tmpStart.x, tmpNow.x);
-		m_dvec3MinBound.y = std::min(tmpStart.y, tmpNow.y);
-		m_dvec3MinBound.z = std::numeric_limits<double>::max();
-
-		m_dvec3MaxBound.x = std::max(tmpStart.x, tmpNow.x);
-		m_dvec3MaxBound.y = std::max(tmpStart.y, tmpNow.y);
-		m_dvec3MaxBound.z = -std::numeric_limits<double>::max();
-
-		for (auto & ds : m_pDataVolumeDisplay->getDatasets())
+	if (m_bActive)
+	{
+		if (m_bRayHitDomain)
 		{
-			SonarPointCloud* pc = static_cast<SonarPointCloud*>(ds);
-			for (unsigned int i = 0; i < pc->getPointCount(); ++i)
+			if (!m_bSelectingArea && !m_bMovingArea)
 			{
-				glm::dvec3 thisRawPt = pc->getRawPointPosition(i);
-
-				if (thisRawPt.x < m_dvec3MinBound.x || thisRawPt.x > m_dvec3MaxBound.x ||
-					thisRawPt.y < m_dvec3MinBound.y || thisRawPt.y > m_dvec3MaxBound.y)
+				if (m_bRayHitCustomDomain)
 				{
-					pc->markPoint(i, 1);
+					m_vec3BeginDragOnPlane = m_vec3CurrentLocationOnPlane;
+					m_dvec3MinBoundAtDragStart = m_dvec3MinBound;
+					m_dvec3MaxBoundAtDragStart = m_dvec3MaxBound;
+					m_bMovingArea = true;
 				}
 				else
 				{
-					pc->markPoint(i, 0);
-					if (pc->getRawPointPosition(i).z < m_dvec3MinBound.z)
-						m_dvec3MinBound.z = pc->getRawPointPosition(i).z;
-					else if (pc->getRawPointPosition(i).z > m_dvec3MaxBound.z)
-						m_dvec3MaxBound.z = pc->getRawPointPosition(i).z;
+					m_vec3BeginBoundOnPlane = m_vec3CurrentLocationOnPlane;
+					m_bSelectingArea = true;
+				}			
+			}
+
+			if (m_bSelectingArea)
+			{
+				m_vec3EndBoundOnPlane = m_vec3CurrentLocationOnPlane;
+				
+				glm::dvec3 domainCoordsStart = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3BeginBoundOnPlane);
+				glm::dvec3 domainCoordsNow = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3EndBoundOnPlane);
+
+				m_dvec3MinBound.x = std::min(domainCoordsStart.x, domainCoordsNow.x);
+				m_dvec3MinBound.y = std::min(domainCoordsStart.y, domainCoordsNow.y);
+
+				m_dvec3MaxBound.x = std::max(domainCoordsStart.x, domainCoordsNow.x);
+				m_dvec3MaxBound.y = std::max(domainCoordsStart.y, domainCoordsNow.y);
+
+				m_bCustomAreaSet = true;
+			}
+
+			if (m_bMovingArea)
+			{
+				glm::dvec3 domainCoordsStart = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3BeginDragOnPlane);
+				glm::dvec3 domainCoordsNow = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3CurrentLocationOnPlane);
+				glm::dvec3 offset = domainCoordsNow - domainCoordsStart;
+
+				m_dvec3MinBound.x = m_dvec3MinBoundAtDragStart.x + offset.x;
+				m_dvec3MinBound.y = m_dvec3MinBoundAtDragStart.y + offset.y;
+
+				m_dvec3MaxBound.x = m_dvec3MaxBoundAtDragStart.x + offset.x;
+				m_dvec3MaxBound.y = m_dvec3MaxBoundAtDragStart.y + offset.y;
+
+				glm::vec3 markerOffsets = m_vec3CurrentLocationOnPlane - m_vec3BeginDragOnPlane;
+				m_pDataVolumeSelection->convertToRawDomainCoords(m_dvec3MinBoundAtDragStart);
+				m_pDataVolumeSelection->convertToRawDomainCoords(m_dvec3MaxBoundAtDragStart);
+			}	
+
+			m_dvec3MinBound.z = std::numeric_limits<double>::max();
+			m_dvec3MaxBound.z = -std::numeric_limits<double>::max();
+
+			for (auto & ds : m_pDataVolumeDisplay->getDatasets())
+			{
+				SonarPointCloud* pc = static_cast<SonarPointCloud*>(ds);
+				for (unsigned int i = 0; i < pc->getPointCount(); ++i)
+				{
+					glm::dvec3 thisRawPt = pc->getRawPointPosition(i);
+
+					if (thisRawPt.x < m_dvec3MinBound.x || thisRawPt.x > m_dvec3MaxBound.x ||
+						thisRawPt.y < m_dvec3MinBound.y || thisRawPt.y > m_dvec3MaxBound.y)
+					{
+						pc->markPoint(i, 1);
+					}
+					else
+					{
+						pc->markPoint(i, 0);
+						if (pc->getRawPointPosition(i).z < m_dvec3MinBound.z)
+							m_dvec3MinBound.z = pc->getRawPointPosition(i).z;
+						else if (pc->getRawPointPosition(i).z > m_dvec3MaxBound.z)
+							m_dvec3MaxBound.z = pc->getRawPointPosition(i).z;
+					}
 				}
 			}
-		}
 
-		m_pDataVolumeDisplay->setCustomBounds(m_dvec3MinBound, m_dvec3MaxBound);
-		m_pDataVolumeDisplay->useCustomBounds(true);
+			m_pDataVolumeDisplay->setCustomBounds(m_dvec3MinBound, m_dvec3MaxBound);
+			m_pDataVolumeDisplay->useCustomBounds(true);					
+
+			m_vec3LastSelectedLocationOnPlaneWithinDomain = m_vec3CurrentLocationOnPlane;
+		}
+	}
+	else
+	{
+		m_bSelectingArea = false;
+		m_bMovingArea = false;
 	}
 }
 
 void SelectAreaBehavior::draw()
 {
-	if (m_bSelectingArea)
-	{
-		glm::mat4 trans(glm::translate(glm::mat4(), m_vec3StartLocationOnPlane) * glm::scale(glm::mat4(), m_vec3CursorSize));
-
-		Renderer::getInstance().drawPrimitive("icosphere", trans, glm::vec4(1.f, 0.f, 0.f, 1.f), glm::vec4(1.f, 1.f, 1.f, 1.f), 10.f);
-	}
-
 	if (m_bShowCursor)
 	{
+		glm::vec4 pointerColor;
+		
+		if (m_bActive)
+			pointerColor = glm::vec4(1.f, 0.f, 0.f, 0.15f);
+		else
+		{
+			if (m_bRayHitCustomDomain)
+				pointerColor = glm::vec4(0.f, 1.f, 0.f, 0.25f);
+			else
+				pointerColor = glm::vec4(1.f, 1.f, 1.f, 0.25f);
+		}
+
+		// cursor dot
 		glm::mat4 trans(glm::translate(glm::mat4(), m_vec3CurrentLocationOnPlane) * glm::scale(glm::mat4(), m_vec3CursorSize));
 
-		Renderer::getInstance().drawPrimitive("icosphere", trans, glm::vec4(1.f), glm::vec4(1.f), 10.f);
+		Renderer::getInstance().drawPrimitive("icosphere", trans, pointerColor, glm::vec4(1.f), 10.f);
 
+		// connector
 		float connectorRadius = 0.005f;
 
 		glm::vec3 controllerToCursorVec = m_vec3CurrentLocationOnPlane - glm::vec3(m_pPrimaryController->getPose()[3]);
@@ -103,11 +167,33 @@ void SelectAreaBehavior::draw()
 		trans *= rotMat;
 		trans *= glm::scale(glm::mat4(), glm::vec3(connectorRadius, connectorRadius, glm::length(controllerToCursorVec)));
 
-		Renderer::getInstance().drawPrimitive("cylinder", trans, glm::vec4(1.f, 1.f, 1.f, 0.25f), glm::vec4(1.f), 10.f);
+		Renderer::getInstance().drawPrimitive("cylinder", trans, pointerColor, glm::vec4(1.f), 10.f);
 	}
 
-	if (m_pDataVolumeDisplay->getUseCustomBounds())
+	if (m_bSelectingArea)
 	{
+		// MARKER DOTS
+		glm::vec4 markerColor = m_bSelectingArea ? glm::vec4(1.f, 0.f, 0.f, 1.f) : glm::vec4(1.f, 1.f, 1.f, 1.f);
+
+		glm::mat4 trans = glm::translate(glm::mat4(), m_vec3BeginBoundOnPlane) * glm::scale(glm::mat4(), m_vec3CursorSize);
+		Renderer::getInstance().drawPrimitive("icosphere", trans, markerColor, glm::vec4(1.f), 10.f);
+
+		trans = glm::translate(glm::mat4(), m_vec3EndBoundOnPlane) * glm::scale(glm::mat4(), m_vec3CursorSize);
+		Renderer::getInstance().drawPrimitive("icosphere", trans, markerColor, glm::vec4(1.f), 10.f);
+	}
+
+	if (m_bCustomAreaSet)
+	{
+		// CUSTOM AREA BBOX
+		glm::vec4 bboxColor;
+		
+		if (m_bSelectingArea)
+			bboxColor = glm::vec4(0.f, 1.f, 1.f, 1.f);
+		else if (m_bRayHitCustomDomain)
+			bboxColor = glm::vec4(0.f, 1.f, 0.f, 1.f);
+		else
+			bboxColor = glm::vec4(1.f, 1.f, 0.f, 1.f);
+
 		glm::dvec3 midPt = m_pDataVolumeSelection->convertToWorldCoords(m_pDataVolumeDisplay->getCustomMinBound() + m_pDataVolumeDisplay->getCustomDomainDimensions() * 0.5);
 
 		glm::vec3 tmp = m_pDataVolumeDisplay->getCustomDomainDimensions() / m_pDataVolumeSelection->getDataDimensions();
@@ -116,8 +202,27 @@ void SelectAreaBehavior::draw()
 		glm::vec3 customScalingFactors = domainCustomAdjustedVolumeDims * tmp;
 
 		glm::mat4 trans = glm::translate(glm::mat4(), glm::vec3(midPt)) * glm::mat4_cast(m_pDataVolumeSelection->getOrientation()) * glm::scale(glm::mat4(), customScalingFactors);
-		Renderer::getInstance().drawFlatPrimitive("bbox_lines", trans, glm::vec4(0.f, 1.f, 1.f, 1.f));
+		Renderer::getInstance().drawFlatPrimitive("bbox_lines", trans, bboxColor);
 	}
+}
+
+void SelectAreaBehavior::reset()
+{
+	m_dvec3MinBound = glm::dvec3(std::numeric_limits<double>::max());
+	m_dvec3MaxBound = glm::dvec3(-std::numeric_limits<double>::max());
+
+	for (auto & ds : m_pDataVolumeDisplay->getDatasets())
+	{
+		SonarPointCloud* pc = static_cast<SonarPointCloud*>(ds);
+		for (unsigned int i = 0; i < pc->getPointCount(); ++i)
+			pc->markPoint(i, 0);
+	}
+
+	m_pDataVolumeDisplay->setCustomBounds(m_dvec3MinBound, m_dvec3MaxBound);
+	m_pDataVolumeDisplay->useCustomBounds(false);
+	m_bCustomAreaSet = false;
+	m_bSelectingArea = false;
+	m_bMovingArea = false;
 }
 
 void SelectAreaBehavior::receiveEvent(const int event, void * payloadData)
@@ -130,17 +235,7 @@ void SelectAreaBehavior::receiveEvent(const int event, void * payloadData)
 		memcpy(&payload, &payloadData, sizeof(BroadcastSystem::Payload::Trigger*));
 		if (payload->m_pSelf == m_pPrimaryController)
 		{
-			glm::vec3 planeOrigin = m_pDataVolumeSelection->getPosition() + glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 0.5f)) * m_pDataVolumeSelection->getDimensions().z;
-			glm::vec3 planeNormal = glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 1.f));
-			glm::vec3 rayOrigin = glm::vec3(m_pPrimaryController->getPose()[3]);
-			glm::vec3 rayDirection = glm::normalize(glm::vec3(-m_pPrimaryController->getPose()[2]));
-
-			if (castRay(rayOrigin, rayDirection, planeOrigin, planeNormal, &m_vec3StartLocationOnPlane) && m_pDataVolumeSelection->isWorldCoordPointInBounds(m_vec3StartLocationOnPlane, false))
-			{
-				m_dvec3MinBound = m_pDataVolumeSelection->convertToRawDomainCoords(m_vec3StartLocationOnPlane);
-				std::cout << "(" << m_dvec3MinBound.x << ", " << m_dvec3MinBound.y << ", " << m_dvec3MinBound.z << ")" << std::endl;
-				m_bSelectingArea = true;
-			}
+			m_bActive = true;
 		}
 
 		break;
@@ -151,7 +246,7 @@ void SelectAreaBehavior::receiveEvent(const int event, void * payloadData)
 		memcpy(&payload, &payloadData, sizeof(BroadcastSystem::Payload::Trigger*));
 		if (payload->m_pSelf == m_pPrimaryController)
 		{
-			m_bSelectingArea = false;
+			m_bActive = false;
 		}
 
 		break;
@@ -162,7 +257,7 @@ void SelectAreaBehavior::receiveEvent(const int event, void * payloadData)
 		memcpy(&payload, &payloadData, sizeof(BroadcastSystem::Payload::Touchpad*));
 		if (payload->m_pSelf == m_pPrimaryController)
 		{
-			m_pDataVolumeDisplay->useCustomBounds(false);
+			reset();
 		}
 
 		break;
@@ -170,6 +265,40 @@ void SelectAreaBehavior::receiveEvent(const int event, void * payloadData)
 	default:
 		break;
 	}
+}
+
+glm::vec3 SelectAreaBehavior::calcHits()
+{
+	glm::vec3 planeOrigin = m_pDataVolumeSelection->getPosition() + glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 1.f)) * m_pDataVolumeSelection->getDimensions().z * 0.5f;
+	glm::vec3 planeNormal = glm::rotate(m_pDataVolumeSelection->getOrientation(), glm::vec3(0.f, 0.f, 1.f));
+	glm::vec3 rayOrigin = glm::vec3(m_pPrimaryController->getPose()[3]);
+	glm::vec3 rayDirection = glm::normalize(glm::vec3(-m_pPrimaryController->getPose()[2]));
+	glm::vec3 ptOnPlane;
+
+	m_bRayHitPlane = castRay(rayOrigin, rayDirection, planeOrigin, planeNormal, &ptOnPlane);
+	m_bRayHitDomain = m_bRayHitPlane ? m_pDataVolumeSelection->isWorldCoordPointInBounds(ptOnPlane, false) : false;
+
+	if (m_bRayHitDomain)
+	{
+		if (m_bCustomAreaSet)
+		{
+			glm::dvec3 hitPtDataCoords = m_pDataVolumeSelection->convertToRawDomainCoords(ptOnPlane);
+
+			if (hitPtDataCoords.x > m_dvec3MinBound.x && hitPtDataCoords.x < m_dvec3MaxBound.x &&
+				hitPtDataCoords.y > m_dvec3MinBound.y && hitPtDataCoords.y < m_dvec3MaxBound.y)
+				m_bRayHitCustomDomain = true;
+			else
+				m_bRayHitCustomDomain = false;
+		}
+		else
+			m_bRayHitCustomDomain = false;
+	}
+	else
+	{
+		m_bRayHitCustomDomain = false;
+	}
+
+	return ptOnPlane;
 }
 
 bool SelectAreaBehavior::castRay(glm::vec3 rayOrigin, glm::vec3 rayDirection, glm::vec3 planeOrigin, glm::vec3 planeNormal, glm::vec3 * locationOnPlane)
--- VRSonarCleaner/SelectAreaBehavior.h
+++ VRSonarCleaner/SelectAreaBehavior.h
@@ -14,23 +14,40 @@ public:
 
 	void draw();
 
+	void reset();
+
 private:
 	DataVolume* m_pDataVolumeSelection;
 	DataVolume* m_pDataVolumeDisplay;
 
-	glm::dvec3 m_dvec3MinBound;
-	glm::dvec3 m_dvec3MaxBound;
+	bool m_bActive; // has the pointer been activated by the user?
+	bool m_bShowCursor; // show the cursor?
+	bool m_bSelectingArea; // is an area currently being selected?
+	bool m_bMovingArea; // is the custom area currently being moved around?
+	bool m_bCustomAreaSet; // has a custom domain been set?
+	bool m_bRayHitPlane;
+	bool m_bRayHitDomain;
+	bool m_bRayHitCustomDomain;
 
-	bool m_bShowCursor;
-	bool m_bSelectingArea;
-	glm::vec3 m_vec3StartLocationOnPlane;
-	glm::vec3 m_vec3CurrentLocationOnPlane;
+	glm::vec3 m_vec3CurrentLocationOnPlane; // world pos of cursor on plane
+	glm::vec3 m_vec3LastSelectedLocationOnPlaneWithinDomain;
+
+	glm::dvec3 m_dvec3MinBound; // raw data coordinated for min boundary
+	glm::dvec3 m_dvec3MaxBound; // raw data coordinated for max boundary
+
+	glm::vec3 m_vec3BeginBoundOnPlane; // world pos of bbox start on selection plane
+	glm::vec3 m_vec3EndBoundOnPlane; // world pos of current bbox bound on selection plane
+
+	glm::vec3 m_vec3BeginDragOnPlane; // world pos of cursor on selection plane when dragging action started
+	glm::dvec3 m_dvec3MinBoundAtDragStart;
+	glm::dvec3 m_dvec3MaxBoundAtDragStart;
 	
 	glm::vec3 m_vec3CursorSize;
 
 private:
 	void receiveEvent(const int event, void* payloadData);
 
+	glm::vec3 calcHits();
 	bool castRay(glm::vec3 rayOrigin, glm::vec3 rayDirection, glm::vec3 planeOrigin, glm::vec3 planeNormal, glm::vec3* locationOnPlane);
 };
 
