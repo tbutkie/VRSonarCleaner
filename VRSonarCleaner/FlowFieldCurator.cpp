9a6fd71f7c8334692b85aa9b07df3dfa7c52e901
drew <ahstevens@gmail.com>
2018-08-13 13:32:21 -0400

    flowgrid metafile loading

--- VRSonarCleaner/FlowFieldCurator.cpp
+++ VRSonarCleaner/FlowFieldCurator.cpp
@@ -1,5 +1,9 @@
 #include "FlowFieldCurator.h"
 
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+
 using namespace std::chrono_literals;
 
 FlowFieldCurator::FlowFieldCurator(TrackedDeviceManager* pTDM, FlowVolume* flowVol)
@@ -32,7 +36,59 @@ bool FlowFieldCurator::loadRandomFlowGrid()
 	if (system("resources\\data\\flowgrid\\VecFieldGen.exe -outfile flowgrid_test.fg") == EXIT_SUCCESS)
 	{
 		m_pFlowVolume->addFlowGrid("flowgrid_test.fg", true);
+		loadMetaFile("flowgrid_test.fg.cp");
 	}
 
 	return false;
+}
+
+void FlowFieldCurator::loadMetaFile(std::string metaFileName)
+{
+	using namespace std::experimental::filesystem::v1;
+
+	std::ifstream cpFile;
+
+	cpFile.open(metaFileName);
+
+	std::unordered_map<std::string, ControlPoint> CPMap;
+
+	if (cpFile.is_open())
+	{
+		std::string line;
+		std::vector<float> cpvals;
+
+		while (std::getline(cpFile, line))
+		{
+			std::stringstream ss(line);
+
+			std::string cpName, cpAttr;
+			std::getline(ss, cpName, '_');
+			std::getline(ss, cpAttr, ',');
+
+			std::string cell;
+			std::vector<float> vals;
+
+			while (std::getline(ss, cell, ','))
+			{
+				float tmpVal = std::stof(cell);
+				vals.push_back(tmpVal);
+			}
+
+			if (vals.size() == 3)
+			{
+				if (cpAttr.compare("POINT") == 0)
+					CPMap[cpName].pos = glm::vec3(vals[0], vals[1], vals[2]);
+				if (cpAttr.compare("DIRECTION") == 0)
+					CPMap[cpName].dir = glm::vec3(vals[0], vals[1], vals[2]);
+				if (cpAttr.compare("LAMBDA") == 0)
+					CPMap[cpName].lamda = glm::vec3(vals[0], vals[1], vals[2]);
+			}
+			else
+				continue;
+		}
+
+		cpFile.close();
+
+		m_vCPs = CPMap;
+	}
 }
--- VRSonarCleaner/FlowFieldCurator.h
+++ VRSonarCleaner/FlowFieldCurator.h
@@ -2,6 +2,7 @@
 #include "BehaviorBase.h"
 #include "TrackedDeviceManager.h"
 #include "FlowVolume.h"
+#include <unordered_map>
 
 class FlowFieldCurator :
 	public BehaviorBase
@@ -18,7 +19,16 @@ private:
 	TrackedDeviceManager* m_pTDM;
 	FlowVolume* m_pFlowVolume;
 
+	struct ControlPoint {
+		glm::vec3 pos;
+		glm::vec3 dir;
+		glm::vec3 lamda;
+	};
+
+	std::unordered_map<std::string, ControlPoint> m_vCPs;
+
 private:
 	bool loadRandomFlowGrid();
+	void loadMetaFile(std::string metaFileName);
 };
 
